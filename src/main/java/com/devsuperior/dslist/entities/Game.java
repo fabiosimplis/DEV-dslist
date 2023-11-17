e36c45bf9b181bb0b4afc3ed19eec9ca2a52bea5
fabiosimplis <fabio.simplis@gmail.com>
2023-11-17 19:07:09 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -15,20 +15,25 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private String score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")//Para gerar Texto, pois o varchar só vai até 255
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")//Para gerar Texto, pois o varchar só vai até 255
     private String longDescription;
 
     public Game(){
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, String score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -66,14 +71,21 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String platform) {
+        this.score = score;
+    }
     public String getImgUrl() {
         return imgUrl;
     }
